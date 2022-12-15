21865af1d6fb44ceeda0372ac34acf3c870479db
gneris177 <gneris177@gmail.com>
2022-12-14 23:19:04 -0300

    Feat - Criando módulo users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataSource } from 'typeorm';
+import { UsersModule } from './users/users.module';
 import { typeorm } from './config/typeorm';
 
 @Module({
-  imports: [typeorm],
+  imports: [typeorm, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
